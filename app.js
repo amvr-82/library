06afd89d32a2865ea9aa52b81fdc8acff3028e85
amirhossein <amirhossein.develope@gmail.com>
2025-10-29 13:22:34 +0330

    btn load

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const BtnBuild = document.getElementById("btn-build");
-function openmodal() {
-  BtnBuild.classList.add;
+const ModalBuild = document.getElementById("modal-screen");
+function Open_modal() {
+  ModalBuild.classList.remove("hidden");
+  console.log("hi");
 }
 
-BtnBuild.addEventListener(onclick, openmodal());
+BtnBuild.addEventListener("click", Open_modal());
