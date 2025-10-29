72f6bad94459ba0cdd00d7bc600c88bf869d29aa
amirhossein <amirhossein.develope@gmail.com>
2025-10-29 13:51:09 +0330

    js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const BtnBuild = document.getElementById("btn-build");
 const ModalBuild = document.getElementById("modal-screen");
+const CloseModal = document.getElementById("close-modal");
 function Open_modal() {
   ModalBuild.classList.remove("hidden");
-  console.log("hi");
 }
-
-BtnBuild.addEventListener("click", Open_modal());
+function close_modal() {
+  ModalBuild.classList.add("hidden");
+}
+document.addEventListener("click", close_modal());
+CloseModal.addEventListener("click", close_modal());
